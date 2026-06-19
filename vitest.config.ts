117064889adfced7c67d3188a0c529004e09eae3
Darshil Shah <darshils99@gmail.com>
2026-06-19 17:22:03 -0500

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
